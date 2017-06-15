b6fdf0f01f2f630b3eec4afab01db39fa0600e80
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-06-15 16:48:11 -0300

    Primeiro Commit

---
+++ CursoEmVideo/desafio004.py
@@ -0,0 +1,6 @@
+n = input('Digite Algo: ')
+
+print('É Maiusculas ? {}'.format(n.isupper())) #Verifica se é maiuscula
+print('É Numero ? {}'.format(n.isnumeric())) #Verifica se é um numero
+print('É Letra ? {}'.format(n.isalpha())) #Verifica se é letra
+print('É AlfaNumerico ? {}'.format(n.isalnum())) #Verifica se é alfa numerico
