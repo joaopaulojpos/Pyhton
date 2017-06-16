9d866dcfafe4b5d7271c62ff3537162f3de02c2d
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-06-15 22:42:43 -0300

    Começando a estudar pela documentação

---
+++ Doc/numeros.py
@@ -0,0 +1,15 @@
+#print((50 - 5 * 6) / 4)
+#print(4 / 2) #divisão sempre retorna um número de ponto flutuante
+
+#print(17 /3) #divisão clássica retorna um flutuador
+#print(17 // 3) #divisão descarta a parte fracionada
+#print(17 % 3) #% este operador retorna o resto da divisão
+#print(5 ** 2) #** este operador retorna a potencia
+
+'''
+largura = 20
+altura = 5* 9
+print(largura * altura)
+'''
+
+#print(4 * 3.75 - 1)
---
+++ Doc/strings.py
@@ -0,0 +1,26 @@
+#print('Ovos de Spam') #abordagem Simples
+#print('doesn \'t') #use \' para colocar um scape da citação única
+#print("não") #ou use também aspas duplas
+#print('"Sim", ele disse')
+#print('"Isn \' t", ela disse')
+
+#s = 'Primeira Linha. \n Segunda Linha. ' # \n pula uma linha
+#print(s)
+
+#print('C: \ some \name')
+#print(r'C: \ some \name') #utiliza o R para não interpretar os caracteres especias
+
+print(""" \
+Usage: thingy [OPTIONS]
+    -h      Exibir esta mensagem de uso
+    -H      Hostname para se conectar
+""")
+
+print(3 * 'un' + 'io') # o * significa que o un vai se repetir 3 vezes
+print('Py' 'thon') #duas strings ao lado, são automaticamentes concatenadas
+prefixo = 'Py'
+print(prefixo + 'thon')
+
+texto = ('Coloque várias Strings entre parênteses'
+         'para que elas se juntem.')
+print(texto)
