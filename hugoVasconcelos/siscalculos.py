bd92ef1a42f6dd403bda818576958772b5aea5fd
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-06-19 21:29:29 -0300

    Iniciando curso do Hugo Vasconcelos

---
+++ hugoVasconcelos/siscalculos.py
@@ -0,0 +1,9 @@
+#Sistemas de cálculos
+
+x = int(input('Digite o primeiro valor: '))
+y = int(input('Digite o segundo valor: '))
+
+print('A soma de {} e {} é {}'.format(x, y, x + y))
+print('A Subtração de {} e {} é {}'.format(x, y, x - y))
+print('A Multiplicação de {} e {} é {}'.format(x, y, x * y))
+print('A Divisão de {} e {} é {}'.format(x, y, x / y))
