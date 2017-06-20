0236517829021cc1a209c590dc06ecd64f98adfa
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-06-19 21:27:00 -0300

    Começando curso da Solyd, aula2 e execicio aula 2 feitos

---
+++ Solyd/aula2-entrada-e-saida.py
@@ -0,0 +1,9 @@
+numero1 = 16 # entrada de numero 1
+numero2 = 2
+numero3 = 5.5
+
+resultado = numero1 ** (1/2) # pega a raiz quadrada do numero1
+
+print(resultado)
+
+
---
+++ Solyd/exercicios/ex-aula2.py
@@ -0,0 +1,20 @@
+'''
+EXERCICIO: Faça um formulario que pergunte
+o nome, cpf, endereco, idade, altura e telefone
+e imprima em um relatorio organizado
+'''
+
+nome = input('Digite seu nome: ')
+cpf = input('Digite seu cpf: ')
+endereco = input('Digite seu endereço: ')
+idade = input('Digite sua idade: ')
+altura = input('Digite sua altura: ')
+telefone = input('Digite seu telefone: ')
+
+print('------ Seus dados são ------')
+print('Nome:', nome)
+print('CPF:', cpf)
+print('Endereço:', endereco)
+print('Idade:', idade, 'anos')
+print('Altura:', altura, 'metros')
+print('Telefone:', telefone)
