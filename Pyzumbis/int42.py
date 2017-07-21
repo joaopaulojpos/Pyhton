b131950e85066637b1460eb4ab7408fbd33fb031
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-07-20 23:11:43 -0300

    Exemplo pratico basico com OO

---
+++ Pyzumbis/int42.py
@@ -0,0 +1,16 @@
+class int42(int):
+    def __init__(self, n):
+        int.__init__(n)
+
+    def __add__(a, b):
+        return 42
+
+    def __str__(n):
+        return '42'
+
+a = int42(7)
+b = int42(13)
+
+print(a + b)
+print(a)
+print(b)
