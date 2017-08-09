49672141ca2bdbb2fa0a9854225707e8e73e6381
João Paulo Oliveira <jpo.oliveiras@gmail.com>
2017-08-09 14:40:24 -0300

    Arquivos do curso de python da USP

---
+++ temperatura.py
@@ -0,0 +1,5 @@
+temperatura_fahrenheit = float(input("Digite uma temperatura em Fahrenheit: "))
+
+temperatura_celsius = (temperatura_fahrenheit - 32) * 5 / 9
+
+print("A temperatura em celsius é ", temperatura_celsius)
---
+++ variaveis.py
@@ -0,0 +1,30 @@
+# a = 10
+# b = 20
+#
+# soma = a + b
+# print('A soma dos números é', soma)
+
+# soma = 5.5
+# print('soma =', soma)
+
+# x = 10
+# y = 30
+# x = x + 10
+# y = x + 10
+# x = x + y
+# print(x)
+
+# x = 50
+# y = 20
+# aux = x
+# x = y
+# y = aux
+#
+# print('X', x, 'Y', y)
+
+# a = 1
+# b = 2
+# print(a + 2 * b)
+
+x = 1
+print(x != 1)
