79230ca8db60a6e1c88bce1e569c7138ea6f8d6d
João Paulo Oliveira <jpo.oliveiras@gmail.com>
2017-08-11 15:51:11 -0300

    Exercicio Pratico com if else

---
+++ USP/buzz.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um numero: "))
+resultado = num % 5
+if resultado == 0:
+    print("buzz")
+else:
+    print(num)
---
+++ USP/fizz.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um numero: "))
+resultado = num % 3
+if resultado == 0:
+    print("fizz")
+else:
+    print(num)
